456d6c773b3dc54c0c407d1063aa36effee90309
Ibrahim-Magdi <123883125+Ibrahim-Magdi@users.noreply.github.com>
2023-03-22 11:18:55 +0200

    add JS and CSS files

---
+++ modal/script.js
@@ -0,0 +1,16 @@
+const openbtn = document.querySelector(".js-open")
+const modalbg = document.getElementById("modal-background")
+const modalbox = document.getElementById("modal-box")
+openbtn.addEventListener('click', function(event) {
+    event.preventDefault()
+    modalbg.classList.add("active")
+    modalbox.classList.add("active")
+})
+const closebtn = document.querySelectorAll(".js-close")
+closebtn.forEach(node => {
+    node.addEventListener('click', function (event) {
+        event.preventDefault()
+        modalbg.classList.remove("active")
+        modalbox.classList.remove("active")
+    })
+})
